53c6626e713cf349e8fb6c902a6e50ecf30dd52f
Mitchell Richeson <richeson_mitchell@network.lilly.com>
2018-04-17 10:47:59 -0400

    Initial

---
+++ src/main/java/com/richesoncabinets/hackberry/HackberryApplication.java
@@ -0,0 +1,12 @@
+package com.richesoncabinets.hackberry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackberryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HackberryApplication.class, args);
+	}
+}
---
+++ src/main/java/com/richesoncabinets/hackberry/time/TimeController.java
@@ -0,0 +1,52 @@
+package com.richesoncabinets.hackberry.time;
+
+import java.util.Base64;
+
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+public class TimeController {
+
+	private static final String TSHEETS_TOKEN = "S.4__5bef0f3becb66d52b38539bea60456fa8a76b06a";
+	private static final String URL = "https://jsonplaceholder.typicode.com/posts/1";
+
+	@RequestMapping("/test")
+	public String test() {
+		HttpHeaders headers = new HttpHeaders();
+		headers.add("Authorization", "Bearer " + TSHEETS_TOKEN);
+		
+		HttpEntity<String> request = new HttpEntity<String>(headers);
+		ResponseEntity<String> response = new RestTemplate().exchange("https://rest.tsheets.com/api/v1/timesheets?start_date=%s&end_date=%s&supplemental_data=yes", HttpMethod.GET, request, String.class);
+		Object account = response.getBody();
+		
+		return "success";
+	}
+
+	@RequestMapping("/time")
+	public String getTimesheets(@RequestParam(value = "date", defaultValue = "") String date) {
+
+		HttpHeaders header = new HttpHeaders();
+		header.add("Authorization",
+				Base64.getEncoder().encodeToString(String.format("Bearer %s", TSHEETS_TOKEN).getBytes()));
+
+		HttpEntity<String> request = new HttpEntity<>("parameters", header);
+
+		RestTemplate restTemplate = new RestTemplate();
+		String url = String.format(
+				"https://rest.tsheets.com/api/v1/timesheets?start_date=%s&end_date=%s&supplemental_data=yes", date,
+				date);
+
+		System.out.println(url);
+
+		ResponseEntity<String> s = restTemplate.exchange(URL, HttpMethod.GET, null, String.class);
+		return s.getBody();
+
+	}
+}
