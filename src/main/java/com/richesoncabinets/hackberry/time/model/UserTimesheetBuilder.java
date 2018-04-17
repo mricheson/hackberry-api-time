d6522efdd32520d2b7bda9a51e9e1e17e5ee000c
Mitchell Richeson <richeson_mitchell@network.lilly.com>
2018-04-17 14:08:45 -0400

    Null check on list of timesheets

--- src/main/java/com/richesoncabinets/hackberry/time/model/UserTimesheetBuilder.java
+++ src/main/java/com/richesoncabinets/hackberry/time/model/UserTimesheetBuilder.java
@@ -16,8 +16,9 @@ public class UserTimesheetBuilder {
 	public static List<UserTimesheet> of(Collection<User> users, Collection<Timesheet> timesheets) {
 		Map<Long, List<Timesheet>> userMappedTimesheets = timesheets.stream()
 				.collect(Collectors.groupingBy(Timesheet::getUser_id));
-		
-		userMappedTimesheets.forEach((k,v) -> System.out.println(k + " : "+ v.size()));
+
+		System.out.println(timesheets.size());
+		userMappedTimesheets.forEach((k, v) -> System.out.println(k + " : " + v.size()));
 
 		return users.stream().map(user -> UserTimesheetBuilder.of(user, userMappedTimesheets.get(user.getId())))
 				.collect(Collectors.toList());
@@ -26,9 +27,12 @@ public class UserTimesheetBuilder {
 	public static UserTimesheet of(User user, Collection<Timesheet> timesheets) {
 		UserTimesheet userTimesheet = new UserTimesheet();
 		userTimesheet.setUser(user);
-		userTimesheet.setTimesheets(timesheets.stream().sorted((t1, t2) -> t1.getStart().compareTo(t2.getStart()))
-				.collect(Collectors.toList()));
-		userTimesheet.setClockInTime(userTimesheet.getTimesheets().stream().map(Timesheet::getStart).findFirst().orElse(null));
+		if (timesheets != null) {
+			userTimesheet.setTimesheets(timesheets.stream().sorted((t1, t2) -> t1.getStart().compareTo(t2.getStart()))
+					.collect(Collectors.toList()));
+			userTimesheet.setClockInTime(
+					userTimesheet.getTimesheets().stream().map(Timesheet::getStart).findFirst().orElse(null));
+		}
 		return userTimesheet;
 	}
 }
