40f3307075562b889e4cf8714bec7452fe39a392
Mitchell Richeson <richeson_mitchell@network.lilly.com>
2018-04-26 11:37:20 -0400

    Use exectuor service for making rest calls to tsheets api

--- src/main/java/com/richesoncabinets/hackberry/time/services/TimesheetService.java
+++ src/main/java/com/richesoncabinets/hackberry/time/services/TimesheetService.java
@@ -1,6 +1,9 @@
 package com.richesoncabinets.hackberry.time.services;
 
 import java.time.LocalDate;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
@@ -15,19 +18,31 @@ public class TimesheetService {
 
 	@Autowired
 	private TsheetsService tsheetsService;
-	
+
 	@Autowired
 	private UserTimesheetBuilder userTimesheetBuilder;
 
 	public DailyAttendanceReport getUserTimesheetsForDay(LocalDate date) {
 		String query = String.format("on_the_clock=both&start_date=%1$s&end_date=%1$s&supplemental_data=yes",
 				date.toString());
-		TimesheetsResult timesheets = tsheetsService.getTimesheets(query);
 
-		UsersResult users = tsheetsService.getUsers("");
+		ExecutorService threadExecutor = Executors.newCachedThreadPool();
+		
+		try {
+			Future<TimesheetsResult> timesheetsFuture = threadExecutor
+					.submit(() -> tsheetsService.getTimesheets(query));
+			Future<UsersResult> usersFuture = threadExecutor.submit(() -> tsheetsService.getUsers(""));
 
-		return DailyAttendanceReport.of(userTimesheetBuilder.of(users.getResults().getUsers().values(),
-				timesheets.getResults().getTimesheets().values(), timesheets.getSupplemental_data().getJobcodes()),
-				timesheets.getSupplemental_data().getJobcodes());
+			TimesheetsResult timesheets = timesheetsFuture.get();
+			UsersResult users = usersFuture.get();
+
+			return DailyAttendanceReport.of(userTimesheetBuilder.of(users.getResults().getUsers().values(),
+					timesheets.getResults().getTimesheets().values(), timesheets.getSupplemental_data().getJobcodes()),
+					timesheets.getSupplemental_data().getJobcodes());
+		} catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		}
+
 	}
 }
